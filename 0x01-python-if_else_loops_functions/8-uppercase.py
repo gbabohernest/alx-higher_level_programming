fa1d4fcb7929c5d3bba5b6e1c8f614316903e31b
Ernest <gbaboh.ernest@gmail.com>
2023-05-10 22:32:29 +0000

    fix bug concerning print functions edge case

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,8 +2,7 @@
 
 def uppercase(str):
     for single_char in str:
-        if ord(single_char) >=97 and ord(single_char) <= 122:
-            print("{}".format(chr(ord(single_char) - 32)), end="")
-        else:
-            print("{}".format(single_char), end="")
+        print("{}".format(chr(ord(single_char) - 32)
+                        if (ord(single_char) >= 97 and ord(single_char) <= 122)
+                        else single_char), end="")
     print()
