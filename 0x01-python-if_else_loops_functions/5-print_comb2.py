ec145e232ab1cb10aa3882202aafae84d3d62488
Ernest <gbaboh.ernest@gmail.com>
2023-05-10 14:08:14 +0000

    A script that prints numbers from 0 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,7 +2,7 @@
 
 for num in range(100):
     print("{:02d}".format(num), end="")
-    if num == 99:
-        print()
+    if num < 99:
+        print(", ", end="")
     else:
-       print(end=", ")
+       print()
