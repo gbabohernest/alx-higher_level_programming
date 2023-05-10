3016cb93d0e04fdfe575ecbbcdb59e56be244b08
Ernest <gbaboh.ernest@gmail.com>
2023-05-10 15:33:25 +0000

    refactor code to handle negative edge case

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,9 @@
 import random
 number = random.randint(-10000, 10000)
 
-last_digit = number % 10
+last_digit = abs(number) % 10
+if number < 0:
+    last_digit = -last_digit
 string = 'and is less than 6 and not 0'
 
 if (last_digit > 5):
