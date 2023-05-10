d3296581e316083ae83944e04a73a5f37f9a73e7
Ernest <gbaboh.ernest@gmail.com>
2023-05-10 14:43:04 +0000

    Refactor the code to allow two print function with string format

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 
-for num in range(100):
-    print("{:02d}".format(num), end="")
-    if num < 99:
-        print(", ", end="")
-    else:
-       print()
+for num in range(0, 99):
+    print("{:02d}".format(num), end=", " if num < 98 else ', {}\n'.format(99))
