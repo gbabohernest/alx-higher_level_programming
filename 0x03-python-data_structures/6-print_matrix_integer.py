7b9fc8815b8182bfeafa0b2319ff5ae16a26d858
Ernest <gbaboh.ernest@gmail.com>
2023-05-16 21:49:42 +0000

    Edge cases 5 -7 bug fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,8 +3,9 @@
 def print_matrix_integer(matrix=[[]]):
     
     for row in matrix:
-        for col in row:
-            print("{:d}".format(col), end="")
-            if (col != row[-1]):
-                print("{}".format(" "), end=" ")
+        for index, col in enumerate(row):
+            if (col == row[-1]):
+                print("{:d}".format(col), end="")
+            else:
+                print("{:d}".format(col), end=" ")
         print()
