85e9b36b9f6631a2cc0d96bf86491e42edf7bd14
Ernest <gbaboh.ernest@gmail.com>
2023-05-31 09:11:23 +0000

    A script that prints only integer element of a list

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+  num = 0
+  for item in range(0, x):
+    try:
+      if isinstance(item, int):
+        print("{:d}".format(my_list[item]),end="")
+        num += 1
+    except:
+      pass
+  print()
+  return (num)
