a4b101e658ab9d1e6cda36052604c1d2d4ee53d7
Ernest <gbaboh.ernest@gmail.com>
2023-05-31 20:24:03 +0000

    add if conditions to handle different exceptions instances

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -5,12 +5,13 @@ def list_division(my_list_1, my_list_2, list_length):
 
         try:
             new_list.append(my_list_1[item] / my_list_2[item])
-        except TypeError:
-            print("wrong type")
-        except IndexError:
-            print("out of range")
-        except ZeroDivisionError:
-            print("division by 0")
+        except (TypeError, IndexError, ZeroDivisionError) as error:
+            if (isinstance(error, TypeError)):
+                print("wrong type")
+            elif (isinstance(error, IndexError)):
+                print("out of range")
+            elif (isinstance(error, ZeroDivisionError)):
+                print("division by 0")
             new_list.append(0)
         finally:
             pass
