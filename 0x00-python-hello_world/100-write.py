e0b653e491d0afe2adfe920b4a2adf928d7c6444
Ernest <gbaboh.ernest@gmail.com>
2023-08-09 23:52:37 +0000

    pycodestyle error fix

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -3,5 +3,5 @@
 if __name__ == "__main__":
     import sys
     sys.stderr.write("and that piece of art is useful "
-                    "- Dora Korpar, 2015-10-19\n")
+                     "- Dora Korpar, 2015-10-19\n")
     sys.exit(1)
