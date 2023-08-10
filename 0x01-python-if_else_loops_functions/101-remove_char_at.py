b3f8a7e447adc58e449948b0f6f27f2268e90029
Ernest <gbaboh.ernest@gmail.com>
2023-08-10 22:48:16 +0000

    removes a character in a string at a given position

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def remove_char_at(str, n):
+    result_str = ""
+
+    for i in range(len(str)):
+        if i != n:
+            result_str += str[i]
+    return (result_str)
