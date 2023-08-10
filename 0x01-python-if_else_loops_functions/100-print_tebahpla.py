4ed9b195222500ea322004d6f235ee403bf3329e
Ernest <gbaboh.ernest@gmail.com>
2023-08-10 02:13:33 +0000

    pycodestyle validation eror fix

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
+
 for ch in range(ord('z'), ord('a') - 1, - 1):
     print("{}".format(chr(ch).lower()
-                      if ch % 2 == 0
-                      else chr(ch).upper()), end="")
-
+                if ch % 2 == 0
+                else chr(ch).upper()), end="")
