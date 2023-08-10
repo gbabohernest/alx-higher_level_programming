47989cb078cfceb4757bf858827d3f7b9fab4593
Ernest <gbaboh.ernest@gmail.com>
2023-08-10 18:47:54 +0000

    reads command line argument and print the number, and list of arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import sys
+
+def num_of_args():
+
+    num_of_args = len(sys.argv) - 1
+    arg_placeholder = "arguments"
+    deliminator = ":"
+
+    if (num_of_args == 1):
+        arg_placeholder = "argument"
+    if (num_of_args == 0):
+          deliminator = "."
+
+    print("{} {}{}".format(num_of_args, arg_placeholder, deliminator))
+
+    
+    for count, args in enumerate(sys.argv[1:], start=1):
+        print("{}: {}".format(count, args))
+
+if __name__ == "__main__":
+    num_of_args()
