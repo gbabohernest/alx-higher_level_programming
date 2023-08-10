af02324a091c8d911314c71c74c17f9bbdfecb7f
Ernest <gbaboh.ernest@gmail.com>
2023-08-10 02:00:36 +0000

    prints all possible combination of two digits

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+for first_num in range(0, 10):
+    for second_num in range(first_num+1, 10):
+        print("{}{}".format(first_num, second_num), end="")
+        if first_num < 8:
+            print(", ",end="")
+print()
