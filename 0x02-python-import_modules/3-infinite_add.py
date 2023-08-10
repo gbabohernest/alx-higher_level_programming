46f972ed5c6f1a0977da5c0e44a0cc9a691599a0
Ernest <gbaboh.ernest@gmail.com>
2023-08-10 20:29:34 +0000

    infitie addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+from sys import argv
+
+def add_args():
+    result = 0;
+
+    if len(argv) < 2:
+        print(int(0))
+
+    else:
+        for count in range(1, len(argv)):
+            result +=  int(argv[count])
+    
+        print(result)
+
+if __name__ == "__main__":
+    add_args()
