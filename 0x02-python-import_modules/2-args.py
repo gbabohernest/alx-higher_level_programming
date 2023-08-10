820cbab54f074f8f4bd1b55ed5db3bb231853f59
Ernest <gbaboh.ernest@gmail.com>
2023-08-10 19:39:03 +0000

    output edges case error fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 
-import sys
+from sys import argv
 
 def num_of_args():
-
+    num_of_args = len(argv) - 1
     arg_placeholder = "arguments"
     deliminator = "."
     
-    num_of_args = len(sys.argv) - 1
-
     if (num_of_args == 1):
         arg_placeholder = "argument"
     if (num_of_args > 0):
@@ -17,7 +15,7 @@ def num_of_args():
     print("{} {}{}".format(num_of_args, arg_placeholder, deliminator))
 
     
-    for count, args in enumerate(sys.argv[1:], start=1):
+    for count, args in enumerate(argv[1:], start=1):
         if (count > 0):
             print("{}: {}".format(count, args))
 
