af7d47fe674503c6ef8f3ba1a5d231120b7177d2
Ernest <gbaboh.ernest@gmail.com>
2023-08-10 20:55:50 +0000

    bug fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,22 +2,20 @@
 
 from sys import argv
 
-def num_of_args():
+if __name__ == "__main__":
+
     num_of_args = len(argv) - 1
     arg_placeholder = "arguments"
     deliminator = "."
-    
+
     if (num_of_args == 1):
         arg_placeholder = "argument"
+
     if (num_of_args > 0):
-          deliminator = ":"
+        deliminator = ":"
 
-    print("{} {}{}".format(num_of_args, arg_placeholder, deliminator))
+    print("{} {}{}".format(num_of_args, arg_placeholder,deliminator))
 
-    
     for count, args in enumerate(argv[1:], start=1):
         if (count > 0):
             print("{}: {}".format(count, args))
-
-if __name__ == "__main__":
-    num_of_args()
