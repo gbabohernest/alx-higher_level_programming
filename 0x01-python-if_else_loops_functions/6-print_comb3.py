c8c02d7ac2790aedfd00de4b0dc6c44a547c8e2e
Ernest <gbaboh.ernest@gmail.com>
2023-08-10 02:03:01 +0000

    pycodestyle validation error fix

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,5 +4,5 @@ for first_num in range(0, 10):
     for second_num in range(first_num+1, 10):
         print("{}{}".format(first_num, second_num), end="")
         if first_num < 8:
-            print(", ",end="")
+            print(", ", end="")
 print()
