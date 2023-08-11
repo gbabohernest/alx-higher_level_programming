9440ce9257d9d0fd55d15b87036f1dca486cbe43
Ernest <gbaboh.ernest@gmail.com>
2023-08-11 21:59:01 +0000

    pycodestyle validation error fix

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,17 +2,19 @@
 
 from sys import argv
 
+
 def add_args():
-    result = 0;
+    result = 0
 
     if len(argv) < 2:
         print(int(0))
 
     else:
         for count in range(1, len(argv)):
-            result +=  int(argv[count])
-    
+            result += int(argv[count])
+
         print("{:d}".format(result))
+
 
 if __name__ == "__main__":
     add_args()
