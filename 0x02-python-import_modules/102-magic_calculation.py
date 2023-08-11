630f471975476e95152dc802c4caaf0a3d630e5f
Ernest <gbaboh.ernest@gmail.com>
2023-08-11 17:02:58 +0000

    output error fix

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -8,6 +8,7 @@ def magic_calculation(a, b):
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
+        return c
     else:
         result = sub(a, b)
         return (result)
