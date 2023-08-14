5fcec83a7b298767cab003c755097fca3a5bee4e
Ernest <gbaboh.ernest@gmail.com>
2023-08-14 21:22:10 +0000

    fix output error

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,10 +2,10 @@
 
 def max_integer(my_list=[]):
 
-    max_value = 0
     if len(my_list) < 1:
         return None
     else:
+        max_value = my_list[0]
         for index, num in enumerate(my_list):
             if num > max_value:
                 max_value = num
