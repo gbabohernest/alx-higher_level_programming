ba8c1398b5d56b3a8628a688d2562316ac9cabbb
Ernest <gbaboh.ernest@gmail.com>
2023-08-14 20:58:24 +0000

    fix correct output error

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 
 def multiple_returns(sentence):
 
-    if sentence is not None:
+    if sentence is not None and len(sentence) > 0:
         length = len(sentence)
         result = (length, sentence[0])
         return result
