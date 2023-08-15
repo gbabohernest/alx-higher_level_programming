d64f4fe71435b8af368b53a92cda3f449b50701c
Ernest <gbaboh.ernest@gmail.com>
2023-08-15 20:03:36 +0000

    c function that prints basic info about python list

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,42 @@
+#include <Python.h>
+#include <object.h>
+#include <listobject.h>
+
+/**
+* print_python_list_info - Print basic list info about Python list
+* @p: list object
+* Return: void (nothing)
+*/
+
+void print_python_list_info(PyObject *p)
+{
+	if (p == NULL || !PyList_check(p))
+	{
+		printf("Expected a list");
+		return ();
+	}
+
+	Py_ssize_t sizeOfList = PyList_Size(p);
+
+	printf("[*] Size of the Python List = %ld\n", sizeOfList);
+
+	PyListObject *pObj = (PyListObject *)p;
+
+	Py_ssize_t i;
+
+	printf("[*] Allocated = ld\n", pObj->allocated);
+
+	for (i = 0; i < sizeOfList; i++)
+	{
+		pyObject *item = PyList_GetItem(p, i)
+
+		if (item == NULL)
+		{
+			printf("Element %ld: Unable to get element\n", i);
+			continue;
+		}
+		const char *type_name = Py_TYPE(item)->tp_name;
+
+		printf("Element %ld: %s\n", i, type_name);
+	}
+}
