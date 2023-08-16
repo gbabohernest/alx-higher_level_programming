02de3c1ab95cec814c40123c1a2d23c6a89e9c67
Ernest <gbaboh.ernest@gmail.com>
2023-08-16 22:35:48 +0000

    add all function to check if dictionary is empty or not

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    if len(a_dictionary) > 0:
+    if all([a_dictionary]):
         for key, value in sorted(a_dictionary.items()):
             print("{}: {}".format(key, value))
     else:
