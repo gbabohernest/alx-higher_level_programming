a02b9cef31729d31f787ee28396ad06fd5e77c3e
Ernest <gbaboh.ernest@gmail.com>
2023-08-16 22:37:08 +0000

    refactor, remove the if condition

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -3,8 +3,7 @@
 def update_dictionary(a_dictionary, key, value):
 
     if all([a_dictionary, key, value]):
-        if key in a_dictionary:
-            a_dictionary[key] = value
-        else:
-            a_dictionary[key] = value
+        a_dictionary[key] = value
         return a_dictionary
+    else:
+        return
