41d6efda1583101b5639a38cdaf35bdfdaddb61a
Ernest <gbaboh.ernest@gmail.com>
2023-08-16 21:51:30 +0000

    added print statement

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,10 +2,7 @@
 
 def print_sorted_dictionary(a_dictionary):
     if len(a_dictionary) > 0:
-        sorted_dic = {}
         for key, value in sorted(a_dictionary.items()):
-            sorted_dic[key] = value
-
-        return sorted_dic
+            print("{}: {}".format(key, value))
     else:
-        return {}
+        return
