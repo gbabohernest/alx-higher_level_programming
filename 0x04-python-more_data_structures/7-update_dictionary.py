a146b2651bcf6460d3d7c4c4336a78b9c9fa33f9
Ernest <gbaboh.ernest@gmail.com>
2023-08-16 22:47:57 +0000

    pycodestyle fix

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,7 +2,7 @@
 
 def update_dictionary(a_dictionary, key, value):
 
-    if  isinstance(key, str) and a_dictionary is not None:
+    if isinstance(key, str) and a_dictionary is not None:
         a_dictionary[key] = value
         return a_dictionary
     else:
