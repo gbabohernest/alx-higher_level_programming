051ea82583b943d343ce15285541b1c3052f754c
Ernest <gbaboh.ernest@gmail.com>
2023-08-16 20:45:00 +0000

    return the intersection of two sets

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+
+    if set_1 is not None and set_2 is not None:
+
+        common = set_1 & set_2
+        return common
+
+    return set()
