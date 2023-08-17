db808587abc2b82725be9714ec20a51af967e660
Ernest <gbaboh.ernest@gmail.com>
2023-08-17 00:47:44 +0000

    fix bug with the return statement

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/pthon3
 def multiply_list_map(my_list=[], number=0):
-    return list(map(lambda item: item * number, my_list))
+    return (list(map(lambda item: item * number, my_list)))
