2bde3abe04131f449daf9d7b936f498b118bd33b
Ernest <gbaboh.ernest@gmail.com>
2023-08-17 02:12:23 +0000

    correct output error fix

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,13 +2,15 @@
 
 def weight_average(my_list=[]):
 
-    if my_list is not None:
+    if len(my_list) > 0:
         average_grades = 0
         sum_of_weights = 0
         for items in my_list:
             average_grades += (items[0] * items[1])
-            sum_of_weights += items[-1]
-
-        return (average_grades / sum_of_weights)
+            sum_of_weights += items[1]
+        if sum_of_weights == 0:
+            return 0
+        else:
+            return (average_grades / sum_of_weights)
     else:
         return 0
