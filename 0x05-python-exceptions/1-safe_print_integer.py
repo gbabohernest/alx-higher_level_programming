0a0fdeff320cf20ea53d01765d2868336ac932d9
Ernest <gbaboh.ernest@gmail.com>
2023-08-28 21:19:34 +0000

    check if value isinstance of int

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,13 +4,15 @@
     safe_print_integer - prints an integer with {:d}.format()
     @value: can be any type(int, str, list etc)
     return: True if value is int, false otherwise
-
 """
 
 
 def safe_print_integer(value):
     try:
-        print("{:d}".format(value))
-        return (True)
+        if isinstance(value, int):
+            print("{:d}".format(value))
+            return (True)
+        else:
+            raise ValueError("only integer is alllowed")
     except ValueError:
         return (False)
