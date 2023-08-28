38050cb4bd386a89bff895c2f225b263e5c1cf35
Ernest <gbaboh.ernest@gmail.com>
2023-08-28 20:40:49 +0000

    bug fix

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -11,6 +11,6 @@
 def safe_print_integer(value):
         try:
             print("{:d}".format(value))
-            return True
+            return (True)
         except ValueError as error:
-            return False
+            return (False)
