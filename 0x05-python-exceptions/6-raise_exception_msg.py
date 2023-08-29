1c5f5a4ae6efbb4b8f7dc6ec4dad335547cdf171
Ernest <gbaboh.ernest@gmail.com>
2023-08-29 01:02:22 +0000

    raises a name exception with a message

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""
+    raise_exception_msg - Raises a name exception
+    @message: exception message
+"""
+
+
+def raise_exception_msg(message=""):
+    raise NameError(message)
