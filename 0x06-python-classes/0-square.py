ec9d1feb0d1b289b389ad2cc60f2ffc03170b0d2
Ernest <gbaboh.ernest@gmail.com>
2023-08-29 21:38:39 +0000

    empty square class

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+    An empty square class that
+    defines an empty square
+    instance
+"""
+
+
+class Square:
+    """
+    this function initialize the
+    instance without intial value
+    """
+    """def __init__(self):"""
+    pass
