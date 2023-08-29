d2bc18d54a9fe2c2efd5d77fd4b0806c8b98bc8d
Ernest <gbaboh.ernest@gmail.com>
2023-08-29 21:56:28 +0000

    a class that defines a square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+    A class that defines
+    a square instance
+"""
+
+
+class Square:
+    """
+        create an instance of a private
+        attribute (size)
+        @size: args
+    """
+    def __init__(self, size):
+        self.__size = size
