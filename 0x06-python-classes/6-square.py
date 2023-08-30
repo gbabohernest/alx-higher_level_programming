4ff686c112196116a9a5f7b1c0c7736c34dade6e
Ernest <gbaboh.ernest@gmail.com>
2023-08-30 03:11:55 +0000

    correct output bug fixx with additional space

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -65,7 +65,7 @@ class Square():
     """
     def my_print(self):
         if self.__size == 0:
-            print()
+            print("")
             return
         char = "#"
         for i in range(0, self.__position[1]):
