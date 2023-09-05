d9e1281a23b5caff280791158cb942403513008f
Ernest <gbaboh.ernest@gmail.com>
2023-09-05 06:26:12 +0000

    use the name of the class to access class attribute

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -18,7 +18,7 @@ class Rectangle:
         """
         self.width = width
         self.height = height
-        type(self).number_of_instances += 1
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -86,7 +86,7 @@ class Rectangle:
 
     def __del__(self):
         """prints a message when an instance is deleted"""
-        type(self).number_of_instances -= 1
+        Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
 
     def __ge__(self, obj):
