97160b95136db98f84992bf358f188433091a276
Ernest <gbaboh.ernest@gmail.com>
2023-09-07 01:20:00 +0000

    explicitly checking if a and b are integers or floats

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -14,10 +14,10 @@ def add_integer(a, b=98):
                     b must be an integer
     """
 
-    if (not isinstance(a, float) and not isinstance(a, int)):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
 
-    if (not isinstance(b, float) and not isinstance(b, int)):
+    if not isinstance(b, (int,float):
         raise TypeError("b must be an integer")
 
     int_a = int(a)
