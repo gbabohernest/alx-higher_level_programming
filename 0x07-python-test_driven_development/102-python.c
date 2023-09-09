04b6db36faf4ada81e1554f73b70521797ff04ac
Ernest <gbaboh.ernest@gmail.com>
2023-09-09 08:14:01 +0000

    CPython #3: Python Strings

---
+++ 0x07-python-test_driven_development/102-python.c
@@ -0,0 +1,26 @@
+#include "Python.h"
+
+/**
+ * print_python_string - Prints information about Python strings.
+ * @p: A PyObject string object.
+ */
+void print_python_string(PyObject *p)
+{
+	PyUnicodeObject *unicode;
+	Py_ssize_t length;
+
+	printf("[.] string object info\n");
+
+	if (!PyUnicode_Check(p))
+	{
+		printf("  [ERROR] Invalid String Object\n");
+		return;
+	}
+	unicode = (PyUnicodeObject *)p;
+	length = PyUnicode_GET_LENGTH(p);
+
+	printf("  type: %s\n", (PyUnicode_IS_COMPACT_ASCII(p))
+		? "compact ascii" : "compact unicode object");
+	printf("  length: %ld\n", length);
+	printf("  value: %ls\n", PyUnicode_AS_UNICODE(unicode));
+}
