7dde2dbab52d83078a572c2780337d8ac9ca4693
Ernest <gbaboh.ernest@gmail.com>
2023-09-12 23:56:54 +0000

    add all arguments a list and save as json file

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""This Script add items provided as
+   command line arguments to a Python
+   list and save them in a JSON file.
+"""
+import sys
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+def add_all_args(args, filename):
+    """Adds all arguments to a Python list
+       and save them as a JSON file
+    """
+
+    try:
+        items = load_from_json(filename)
+    except Exception:
+        items = []
+
+    for item in args:
+        items.append(item)
+    save_to_json_file(items, filename)
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    filename = "add_item.json"
+    add_all_args(args, filename)
