efb23b0e5bab3cf5dcbc683e58357a239a2bb3ff
Ernest <gbaboh.ernest@gmail.com>
2023-09-12 08:34:16 +0000

    Reads a text file and prints it to the stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a function that reads a text file"""
+
+
+def read_file(filename=""):
+    """Reads a text file and print
+       its content to the stdout
+
+       Agrs:
+          filename: name of the file to be read
+    """
+    with open(filename, encoding="utf-8") as file_object:
+        print(file_object.read())
