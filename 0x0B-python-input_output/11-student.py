ec80d18e98007bb35a7a033a957176b89e0dda35
Ernest <gbaboh.ernest@gmail.com>
2023-09-13 02:27:04 +0000

    student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""This module defines a Student Class"""
+
+
+class Student:
+    """Defines a student class"""
+    def __init__(self, first_name, last_name, age):
+        """Instantiate with three instance variables
+        args:
+            first_name
+            last_name
+            age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary
+        representation of an instance
+        """
+        filtered_dict = {}
+
+        if attrs is None:
+            return self.__dict__
+
+        if isinstance(attrs, list):
+            for attr in attrs:
+                if isinstance(attr, str) and hasattr(self, attr):
+                    filtered_dict[attr] = getattr(self, attr)
+            return filtered_dict
+
+        return {}
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of student instance"""
+
+        for k, v in json.items():
+            setattr(self, k, v)
