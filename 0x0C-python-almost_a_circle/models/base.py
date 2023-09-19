eaf0d31fb5a827235b2114013e98c8cd8cb324e6
Ernest <gbaboh.ernest@gmail.com>
2023-09-19 02:07:35 +0000

    add static method to_json_string - return json string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """The module defines a Base class for all other classes.
 Its goal is to manage id attribute in all future classes.
 """
+import json
 
 
 class Base:
@@ -13,3 +14,12 @@ class Base:
         else:
             type(self).__nb_objects += 1
             self.id = type(self).__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns the JSON string representation
+        of list_dictionaries
+        """
+        if list_dictionaries is None:
+            return "[]"
+        return json.dumps(list_dictionaries)
