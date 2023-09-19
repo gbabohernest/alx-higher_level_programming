5c2a60c084a8f90e102b8b50365016cd5a316cd1
Ernest <gbaboh.ernest@gmail.com>
2023-09-19 01:26:28 +0000

    add update public instance method  update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -32,3 +32,28 @@ class Square(Rectangle):
         """Print the instance in human-readable format"""
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
                                                  self.width)
+
+    def update(self, *args, **kwargs):
+        """Assigns an argument to each attribute
+               Args:
+                   *args - a tuple of integers to be assigned
+                           as new attribute value
+                   **kwargs - assigns key/value argument to attribute
+        1 -> id attribute
+        2 -> width attribute
+        3 -> height attribute
+        4 -> x attribute
+        5 -> y attribute
+        """
+        attr_names = ["id", "size", "x", "y"]
+        idx_to_attr = {0: 'id', 1: 'size', 2: 'x', 3: 'y'}
+
+        if args and len(args) != 0:
+            for idx, arg in enumerate(args):
+                if idx in idx_to_attr:
+                    setattr(self, idx_to_attr[idx], arg)
+
+        elif kwargs and len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key in attr_names:
+                    setattr(self, key, value)
