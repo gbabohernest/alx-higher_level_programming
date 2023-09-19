325a231a0a88db4ef2954ddcf8cdc5ad90cd37da
Ernest <gbaboh.ernest@gmail.com>
2023-09-19 03:22:03 +0000

    modifies rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,15 +7,19 @@ from models.base import Base
 
 
 class Rectangle(Base):
-    """Defines a Rectangle."""
+    """Defines a Rectangle Class.
+       Inherits from Base.
+       Method:
+            __init__()
+    """
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """instantiate a rectangle."""
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        super().__init__(id)
 
     @property
     def width(self):
