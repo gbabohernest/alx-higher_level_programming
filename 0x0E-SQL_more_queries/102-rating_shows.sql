2b69bcff6e79d44dbe59eb9588a3e978811b74a0
Ernest <gbaboh.ernest@gmail.com>
2023-10-18 22:42:44 +0000

    lists show from db by their rating

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,10 @@
+-- A script that lists all shows from hbtn_0d_tvshows_rate by their rating.
+-- Each record should display: tv_shows.title - rating sum
+-- Results must be sorted in descending order by the rating
+
+SELECT `title`, SUM(`rate`) AS `rating`
+FROM `tv_shows` AS tv
+INNER JOIN `tv_show_ratings` AS rating_tv_show
+ON tv.`id` = rating_tv-show.`show_id`
+GROUP BY `title`
+ORDER BY `rating` DESC;
