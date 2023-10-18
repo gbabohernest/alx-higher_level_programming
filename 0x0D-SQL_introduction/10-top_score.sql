634df540044ad1bce74340843eed993ff1c1c2c2
Ernest <gbaboh.ernest@gmail.com>
2023-10-18 01:20:50 +0000

    list all records of the table second_table

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- A script that lists all record of the table second_table of db hbtn_0c_0
+-- Result should be display in this order score and  name, order by score (top first)
+
+SELECT score, name FROM second_table  ORDER BY score DESC;
