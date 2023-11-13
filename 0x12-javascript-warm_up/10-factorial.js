43a3f30bb308816f7571d9e394d46c23b8f62f34
Ernest <gbaboh.ernest@gmail.com>
2023-11-13 21:42:14 +0000

    Add parseInt function to try and convert the number to integer before checking if it is NAN or not

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -4,14 +4,14 @@ const args = process.argv.slice(2);
 const firstArg = args[0];
 
 const factorialRecursive = (num) => {
-  if (isNaN(num)) {
+  if (isNaN(parseInt(num))) {
     return 1;
   }
-  if (num === 0) {
+  if (parseInt(num) === 0) {
     return 1;
   }
 
-  return num * factorialRecursive(num - 1);
+  return parseInt(num) * factorialRecursive(num - 1);
 };
 
 console.log(factorialRecursive(firstArg));
