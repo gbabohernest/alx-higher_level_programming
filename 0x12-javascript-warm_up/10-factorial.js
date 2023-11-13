e69bd8324c3eef76bedb660c66dc18a31ea637f4
Ernest <gbaboh.ernest@gmail.com>
2023-11-13 22:21:21 +0000

    change the function from arrow to regular function

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,12 +3,12 @@
 const args = process.argv.slice(2);
 const firstArg = parseInt(args[0]);
 
-const factorialRecursive = (num) => {
+function factorialRecursive (num) {
   if (isNaN(num) || num < 1) {
     return 1;
   }
 
   return (num * factorialRecursive(num - 1));
-};
+}
 
 console.log(factorialRecursive(firstArg));
