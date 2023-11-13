00afd9e80fcfd4835aac9028be09d39f54170cfc
Ernest <gbaboh.ernest@gmail.com>
2023-11-13 22:16:09 +0000

    A script that computes and prints a factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+const firstArg = parseInt(args[0]);
+
+const factorialRecursive = (num) => {
+  if (isNaN(num) || num < 1) {
+    return 1;
+  }
+
+  return (num * factorialRecursive(num - 1));
+};
+
+console.log(factorialRecursive(firstArg));
