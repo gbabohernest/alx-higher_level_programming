14dbde39b2e092399a53b94d0658c96fb60ece76
Ernest <gbaboh.ernest@gmail.com>
2023-11-16 23:00:20 +0000

    A script that filter states names

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""This module lists all states with a name starting with N
+   from the database hbtn_0e_0_usa in ascending order using
+   MySQLdb package.
+"""
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+
+    db = MySQLdb.connect(host='localhost', user=argv[1], port=3306,
+                              passwd=argv[2], db=argv[3])
+
+    db_cursor = db.cursor()
+    db_cursor.execute(
+        "SELECT id, name FROM `states` "
+        "WHERE name LIKE 'N%' "
+        "ORDER BY states.id ASC"
+    )
+    rows = db_cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    db_cursor.close()
+    db.close()
