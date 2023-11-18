c4ca5c6920334f99e5b2c29d26fc5aa06ba3794f
Ernest <gbaboh.ernest@gmail.com>
2023-11-18 16:10:25 +0000

    first state model, a file that contains the class definition of a state and inherits from Base, Base = declarative_base()

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This module defines a class State that inherits from
+   Base and link the table states using SQLAlchemy
+"""
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """This class link the MySQL table 'states.'
+    It inherits from Base = declarative_Base using SQLAlchemy
+    """
+    __tablename__ = 'states'
+
+    id = Column('id', Integer, primary_key=True, nullable=False)
+    name = Column('name', String(128), nullable=False)
