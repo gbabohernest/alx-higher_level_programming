7cd5d23684c59942a58cf18f3809de904407a1ca
Ernest <gbaboh.ernest@gmail.com>
2023-11-19 18:47:09 +0000

    city relationship

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Defines a module that creates the State "California with
+   the City "San Francisco from the db hbtn_0e_100_usa using SQLAlchemy
+"""
+
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+
+    Base.metadata.create_all(bind=engine)
+    Session = sessionmaker(bind=engine)
+
+    with Session() as session:
+        session.add(State(name='California',
+                          cities=[City(name='San Francisco')]))
+        session.commit()
---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module defines a class City that inherits from
+   Base and link the table cities using SQLAlchemy
+"""
+
+from relationship_state import Base, State
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+class City(Base):
+    """Defines a class that inherts from Base
+    and link the MQSQL table cities using SQLAlchemy
+    """
+    __tablename__ = "cities"
+    id = Column("id", Integer, primary_key=True, nullable=False)
+    name = Column("name", String(128), nullable=False)
+    state_id = Column("state_id", Integer,
+                      ForeignKey(State.id), nullable=False)
