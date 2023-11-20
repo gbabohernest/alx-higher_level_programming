eeff96c24e544db500a996f2f12db4c380706789
Ernest <gbaboh.ernest@gmail.com>
2023-11-20 18:46:02 +0000

    list relationship

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -19,15 +19,20 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
 
     with Session() as session:
-        result_set = session.query(State, City).order_by(
-            State.id, City.id).filter(State.id
-                                      == City.state_id).all()
+        result_set = session.query(State).order_by(State.id).all()
 
-        curr_state_id = None
+        for state in result_set:
+            print(f"{state.id}: {state.name}")
+            for city in state.cities:
+                print(f"\t{city.id}: {city.name}")
 
-        for state, city in result_set:
-            if state.id != curr_state_id:
-                print(f"{state.id}: {state.name}")
-                curr_state_id = state.id
-
-            print("\t{}: {}".format(city.id, city.name))
+        # result_set = session.query(State, City).order_by(
+        #     State.id, City.id).filter(State.id
+        #                               == City.state_id).all()
+        # curr_state_id = None
+        # for state, city in result_set:
+        #     if state.id != curr_state_id:
+        #         print(f"{state.id}: {state.name}")
+        #         curr_state_id = state.id
+        #
+        #     print("\t{}: {}".format(city.id, city.name))
