c14fef2cf7b2da45c2ce79eb3a8ea5bd1f83b436
Ernest <gbaboh.ernest@gmail.com>
2023-12-01 00:46:35 +0000

    only status code

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a request to the specified URL and displays only the status code of the response
-curl -s -o >(grep -oP '(?<=HTTP/1\.1 )[0-9]+' | head -n1) "$1"
+curl -sI "$1" -w '%{http_code}\n'
