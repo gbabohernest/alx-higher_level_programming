d1cae8dd39415920c023c6f58f639d5e1e9ceec3
Ernest <gbaboh.ernest@gmail.com>
2023-12-01 00:33:26 +0000

    optimized my solution, use the binary search-based approach, which effectively reduces the search space in each iteration, resulting in a logarithmic time complexity of O(log(n)).

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
 """This module defines a function to find
-   peak in list of unsorted integers 
+   peak in list of unsorted integers
 """
 
 
 def find_peak(list_of_integers):
     """Finds a peak in a list of unsorted integers"""
-    if list_of_integers is None or not list_of_integers:
+    if not list_of_integers:
         return None
 
-    for number in range(1, len(list_of_integers) - 1):
-        neighbor_1 = list_of_integers[number - 1]
-        neighbor_2 = list_of_integers[number + 1]
+    low, high = 0, len(list_of_integers) - 1
 
-        if list_of_integers[number] >= neighbor_1 and list_of_integers[number] >= neighbor_2:
-            return list_of_integers[number]
+    while low < high:
+        middle = (low + high) // 2
 
-    return None
+        if list_of_integers[middle] > list_of_integers[middle + 1]:
+            high = middle
+        else:
+            low = middle + 1
+
+    return list_of_integers[low]
