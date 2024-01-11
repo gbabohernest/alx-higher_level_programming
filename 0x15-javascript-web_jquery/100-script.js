635f590076d67208a18e9af97a4e15a5218de8c5
Ernest <gbaboh.ernest@gmail.com>
2024-01-11 02:20:08 +0000

    change text color to red, no jquery, document loaded

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,7 @@
+//Updates text color of <header> element to red
+// script will be loaded in the head tag.
+
+document.addEventListener('DOMContentLoaded', () => {
+  const header = document.querySelector('header');
+  header.style.color = '#FF0000';
+});
